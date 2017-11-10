327d1a5aa36ceec160589ce393db404f1a3048f7
PJ Frias <peejfrias@gmail.com>
2017-11-10 11:03:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ class BoardMember {
   }
 
   sayHi() {
-    console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;)
-    debugger;
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
+    ///console.log(`Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;)
+    ///debugger;
+    ///return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`;
   }
 }
 
